06a92f8683be34ed452ebc2cc6629e8e60d2292e
Ivan Rojo <ivanrojo07@gmail.com>
2017-10-24 16:00:11 -0500

    4:00

--- resources/views/giro/index.blade.php
+++ resources/views/giro/index.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
 	<div class="panel-body">
 		<div class="col-lg-6">
-			<form action="/personal">
+			<form action="#">
 				<div class="input-group">
 					<input type="text" name="query" class="form-control" placeholder="Buscar...">
 					<span class="input-group-btn">
@@ -12,41 +12,37 @@
 				</div>
 			</form>
 		</div>
+		<div class="col-lg-6">
+			<a class="btn btn-success" href="#">Nuevo Giro</a>
+		</div>
 	</div>
 	<div class="jumbotron">
 		<table class="table table-striped table-bordered table-hover" style="color:rgb(51,51,51); border-collapse: collapse; margin-bottom: 0px">
 			<thead>
 				<tr class="info">
-					<th>@sortablelink('nombre', 'Nombre'){{-- Nombre --}}</th>
-					<th>@sortablelink('tipopersona', 'Tipo de persona')</th>
-					<th>@sortablelink('tipo', 'Tipo de cliente')</th>
-					<th>@sortablelink('rfc', 'RFC')</th>
-					<th>@sortablelink('mail', 'Correo') </th>
+					<th>@sortablelink('id', '#'){{-- Nombre --}}</th>
+					<th>@sortablelink('nombre', 'Nombre')</th>
+					<th>@sortablelink('etiqueta', 'Etiqueta')</th>
 					<th>Operacion</th>
 				</tr>
 			</thead>
-			@foreach($personals as $personal)
+			@foreach($giros as $giro)
 				<tr class="active">
 					<td>
-						@if ($personal->tipopersona == "Fisica")
-						{{$personal->nombre}} {{ $personal->apellidopaterno }} {{ $personal->apellidomaterno }}
-						@else
-						{{$personal->razonsocial}}
-						@endif
+						{{ $giro->id }}
 					</td>
-					<td>{{ $personal->tipopersona }}</td>
-					<td>{{ $personal->tipo }}</td>
-					<td>{{ strtoupper($personal->rfc) }}</td>
-					<td>{{$personal->mail}}</td>
+					<td>{{ $giro->nombre }}</td>
+					<td>{{ $giro->etiqueta }}</td>
 					<td>
 						<a class="btn btn-success btn-sm" href="#"><i class="fa fa-eye" aria-hidden="true"></i> Ver</a>
 						<a class="btn btn-info btn-sm" href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar</a>
+						<a class="btn btn-info btn-sm" href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Borrar</a>
 				</tr>
 					</td>
 				</tbody>
 			@endforeach
 		</table>
 	</div>
-	{{ $personals->links()}}
+	{{ $giros->links()}}
 </div>
 @endsection

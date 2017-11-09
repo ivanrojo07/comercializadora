48f18a06b2d3dedc3c20ee155bdf912d457fdad8
Ivan Rojo <ivanrojo07@gmail.com>
2017-11-09 13:06:45 -0600

    vista index

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,3 +1,32 @@
 @extends('layouts.app')
-	@section('content')
-	@endsection
+@section('content')
+	<div class="container">
+		<div class="panel-body">
+			<div class="col-lg-6">
+				<form action="#">
+					<div class="input-group">
+						<input type="text" name="query" class="form-control" placeholder="Buscar...">
+						<span class="input-group-btn">
+							<button class="btn btn-default" type="submit"><i class="fa fa-search" aria-hidden="true"></i>
+							</button>
+						</span>
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="jumbotron">
+			<table class="table table-striped table-bordered table-hover" style="color:rgb(51,51,51); border-collapse: collapse; margin-bottom: 0px">
+				<thead>
+					<tr class="info">
+						<th>@sortablelink('','Identificador')</th>
+						<th>@sortablelink('','Marca')</th>
+						<th>@sortablelink('','Clave')</th>
+						<th>@sortablelink('','Descripción corta')</th>
+						<th>@sortablelink('','Familia')</th>
+						<th>@sortablelink('','Tipo')</th>
+					</tr>
+				</thead>
+			</table>
+		</div>
+	</div>
+@endsection

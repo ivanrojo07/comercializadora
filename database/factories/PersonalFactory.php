6d557e0fc4c5ce7b6d1636bd323c68f676bd9326
Ivan Rojo <ivanrojo07@gmail.com>
2017-11-10 14:16:25 -0600

    faker personal

--- database/factories/PersonalFactory.php
+++ database/factories/PersonalFactory.php
@@ -7,5 +7,14 @@ use Faker\Generator as Faker;
 $factory->define(App\Personal::class, function (Faker $faker) {
     return [
         //
+        'tipopersona' => $faker->randomElement(['Fisica','Moral']);
+        'nombre' => $faker->firstName();
+        'apellidopaterno'=>$faker->lastName();
+        'apellidomaterno'=>$faker->lastName();
+        'razonsocial'=>$faker->word();
+        'alias'=>$faker->word();
+        'rfc'=>$faker->vatId();
+        'vendedor' => $faker->name();
+        'calle'=>
     ];
 });

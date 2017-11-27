586bb60006d8a5cb5906bd9b33f5fc37f66d96d3
Ivan Rojo <ivanrojo07@gmail.com>
2017-11-27 15:49:03 -0600

    Fontawesome

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -63,7 +63,7 @@
       <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <button type="button" class="close" data-dismiss="modal"><i class="fa fa-window-close" aria-hidden="true"></i></button>
           <h4 class="modal-title">Información del Cliente</h4>
         </div>
         <div class="modal-body">

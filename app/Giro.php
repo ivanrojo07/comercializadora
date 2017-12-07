30c39c5676b989eef1c685a98424419c915580c3
Ivan Rojo <ivanrojo07@gmail.com>
2017-12-07 14:16:50 -0600

    soft delete

--- app/Giro.php
+++ app/Giro.php
@@ -3,13 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Kyslik\ColumnSortable\Sortable;
 use Laravel\Scout\Searchable;
 
 class Giro extends Model
 {
     //
-    use Sortable;
+    use Sortable, SoftDeletes;
     protected $table = 'giro';
     protected $fillable=['id','nombre','etiqueta'];
     protected $hidden=[ 'created_at', 'updated_at'];
